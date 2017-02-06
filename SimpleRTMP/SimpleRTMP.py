c699562ec1130252f68b2b2f51ee73f4eb20e0ad
刘超超 <liuchaochao@hoge.cn>
2017-02-06 17:12:30 +0800

    logging configration add root logger

--- SimpleRTMP/SimpleRTMP.py
+++ SimpleRTMP/SimpleRTMP.py
@@ -7,12 +7,15 @@ manager = Manager(app)
 
 @app.route('/')
 def hello_world():
+    logging.getLogger('rtmp').debug('xxx')
     return 'Hello World!'
 
 
 if __name__ == '__main__':
     import logging
+    from logging.config import fileConfig
+    fileConfig('logging.conf')
     try:
         manager.run()
     except KeyboardInterrupt:
-        logging.debug('rtmp services exit')
+        logging.getLogger('rtmp').debug('rtmp services exit')
