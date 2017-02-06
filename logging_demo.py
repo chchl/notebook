f63e1fd904efa4f6c34e4b49a7b6a4d738ca2fcd
刘超超 <liuchaochao@hoge.cn>
2017-02-06 17:12:58 +0800

    logging demo

---
+++ logging_demo.py
@@ -0,0 +1,22 @@
+#-*-coding:UTF-8-*-
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='myapp.log',
+                    filemode='w')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+
+logging.debug('This is a debug message')
+logging.info('This is a info message')
+logging.warning('This is a warning message')
+
+
