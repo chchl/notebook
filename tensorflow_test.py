934f2ae9a7a1b168f041787ae9cc50d2c16e64fa
刘超超 <liuchaochao@hoge.cn>
2017-02-09 16:44:38 +0800

    tensorflow

---
+++ tensorflow_test.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+import tensorflow as tf
+
+a = tf.placeholder("float")
+b = tf.placeholder("float")
+
+y = tf.mul(a,b)
+
+sess = tf.Session()
+
+print sess.run(y, feed_dict={a:3, b:3})
+
+
